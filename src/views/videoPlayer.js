e6d0dff9a9a4c23adcae3fbe72816a26b4a4b083
joshawesome12 <joshawesome12@yahoo.com>
2017-10-17 17:08:44 -0500

    Passing all tests

--- src/views/videoPlayer.js
+++ src/views/videoPlayer.js
@@ -1,25 +1,13 @@
 var VideoPlayerView = Backbone.View.extend({
 
   initialize: function() {
-    // this.videos = new Videos();
-    // // this.collection.on('sync', this.render, this);
-    // this.render();
     this.collection.on('select', this.handleSelect, this);
   }, 
-
-  // this.collection.on('sync', function(response) {
-  //   this.this.model = response[0];
-  //   this.render();
-  // }, this);
-
-  //   this.collection.on('select', this.handleSelect, this);
-  // },    
   
   handleSelect: function(video) {
     this.model = video;
     this.render();
   },
-
   
   render: function() {
     if (this.model) {
